edb77fc29a44214aae69d313fbd756495f9e6bfe
Isaac Sunoo <isaacsunoo@gmail.com>
2019-02-25 18:07:01 -0700

    shuffle options array and add quiz cards

--- src/components/QuizCard.js
+++ src/components/QuizCard.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
 
 class QuizCard extends Component {
-  render() {
-    // console.log(this.props.options);
+  constructor() {
+    super();
+    this.state = {
+      correctQuestions: [],
+      incorrectQuestions: []
+    }
+  }
 
+  displayOptions = () => {
+    this.props.options.map(option => {
+      return <button type='submit'>{option}</button>
+    })
+  }
+
+  determineSolution = (e) => {
+    console.log('taget value', e.target.value);
+    console.log('prototype answer', this.props.proto.prototype);
+    if (this.props.proto.prototype === e.target.value) {
+      this.state.correctQuestions.push(this.props.proto);
+      console.log(this.state.correctQuestions);
+    } else {
+      this.state.incorrectQuestions.push(this.props.proto);
+      console.log(this.state.incorrectQuestions);
+    }
+  }
+
+
+  render() {
     return (
-      <div>
+      <div className='quiz-card'>
         <div className='question-container'>
-          {/* <p>Determine which method ...</p> */}
-          <p>{this.props.proto.answer}</p>
+          <p>{this.props.proto.id}. {this.props.proto.answer}</p>
         </div>
         <div className='answer-container'>
           {this.props.options.map(option => {
-            return <button type='submit'>{option}</button>
+            return <button type='submit' onClick={this.determineSolution} value={option}>{option}</button>
           })}
         </div>
       </div>
--- src/components/pages/QuizPage.js
+++ src/components/pages/QuizPage.js
@@ -10,6 +10,24 @@ class QuizPage extends Component {
     }
   }
 
+
+  // determineSolution = (e) => {
+  //   console.log(e.target.value);
+  //   console.log(this.props.prototype.prototype);
+  //   if (this.props.prototype.prototype === e.target.value) {
+  //     console.log('Correct Answer');
+  //     this.state.correctQuestions.push(this.props.proto);
+  //     console.log(this.state.correctQuestions);
+  //   } else {
+  //     console.log('Incorrect Answer');
+  //     this.state.incorrectQuestions.push(this.props.proto);
+  //     this.props.incorrectCounter++;
+  //     console.log(this.state.incorrectQuestions);
+
+  //   }
+
+  // }
+
   render() {
     return (
       <div>
@@ -18,13 +36,12 @@ class QuizPage extends Component {
         {this.props.prototype.map((proto, idx) => {
           this.state.options.push([proto.prototype, proto.incorrectChoices]);
           let merged = [].concat.apply([], this.state.options[idx]);
-          let currIdx = merged.length, tempVal, randomIdx;
-          randomIdx = Math.floor(Math.random() * currIdx);
-          currIdx--;
-          tempVal = merged[currIdx];
-          merged[currIdx] = merged[randomIdx];
+          let ctr = merged.length, tempVal, randomIdx;
+          randomIdx = Math.floor(Math.random() * ctr);
+          ctr--;
+          tempVal = merged[ctr];
+          merged[ctr] = merged[randomIdx];
           merged[randomIdx] = tempVal;
-          console.log([merged]);
           return <QuizCard key={proto.id} proto={proto} options={merged}/>
         })
         }
